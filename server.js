8aa851cb40fb735ded8079c135d2acc815a33820
frzhilman27 <frzhilman27@gmail.com>
2026-02-25 20:27:12 +0700

    Fix CORS for Vercel

--- server.js
+++ server.js
@@ -9,7 +9,13 @@ const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://sipentar-frontend.vercel.app"
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
