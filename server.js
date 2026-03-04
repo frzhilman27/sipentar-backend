be9054487bc6be230ba264ed9f36e588d7f99e6f
Faris Hilman Ramadhan <frzhi@example.com>
2026-03-05 04:48:20 +0700

    Expand Express payload limits to 10mb to accept massive Base64 Client-Side encoded images

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ const app = express();
    MIDDLEWARE
 ====================== */
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 /* ======================
    STATIC FILES
